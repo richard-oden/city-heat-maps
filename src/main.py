318f604faea48f1c31ab5104e657ea76864488a7
Richard Oden <richard.thomas.oden@gmail.com>
2022-05-30 16:12:51 -0400

    Added code to test functionality of walkscore API.

--- src/main.py
+++ src/main.py
@@ -1,6 +1,15 @@
 from dotenv import load_dotenv
 import os
+import requests
 
 load_dotenv('.env')
 
-walkscore_key = os.environ.get('WALKSCORE_KEY')
+WALKSCORE_KEY = os.environ.get('WALKSCORE_KEY')
+
+def get_walkscore(lat: float, lon: float):
+    response = requests.get(f'https://api.walkscore.com/score?format=json&lat={lat}&lon={lon}&transit=1&bike=1&wsapikey={WALKSCORE_KEY}')
+    if response.ok:
+        return response.json()
+    return
+
+print(get_walkscore(38.328732, -85.764771))
