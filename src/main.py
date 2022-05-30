d2df8f436649d37dee37fb3561e8c3e40d07fa16
Richard Oden <richard.thomas.oden@gmail.com>
2022-05-30 17:46:31 -0400

    Added code to get walkscores for all zipcodes in Louisville.

--- src/main.py
+++ src/main.py
@@ -7,16 +7,21 @@ load_dotenv('.env')
 
 WALKSCORE_KEY = os.environ.get('WALKSCORE_KEY')
 
-def get_zipcodes_by_city(city: str, state: str) -> list:
+def get_zipcode_coordinates_by_city(city: str, state: str) -> dict:
     with SearchEngine() as search:
         results = search.by_city_and_state(city='Louisville', state='Kentucky', returns=None)
-        return [int(result.zipcode) for result in results]
+        return {int(result.zipcode): (result.lat, result.lng) for result in results}
 
-def get_walkscore(lat: float, lon: float):
+
+
+def get_walkscore(lat: float, lon: float) -> dict:
     response = requests.get(f'https://api.walkscore.com/score?format=json&lat={lat}&lon={lon}&transit=1&bike=1&wsapikey={WALKSCORE_KEY}')
     if response.ok:
         return response.json()
     return
 
-print(get_zipcodes_by_city('Louisville', 'Kentucky'))
-print(get_walkscore(38.328732, -85.764771))
+
+walkscores = {zipcode:get_walkscore(coordinates[0], coordinates[1])['walkscore'] 
+    for (zipcode, coordinates) in get_zipcode_coordinates_by_city('Louisville', 'Kentucky').items()}
+
+print(walkscores)
