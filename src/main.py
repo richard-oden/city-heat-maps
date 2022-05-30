4ebfdc61b6226c205df6cb5348c664533ba3b8d2
Richard Oden <richard.thomas.oden@gmail.com>
2022-05-30 17:23:40 -0400

    Added uszipcode and testing code for retrieving zipcodes by city

--- src/main.py
+++ src/main.py
@@ -1,4 +1,5 @@
 from dotenv import load_dotenv
+from uszipcode import SearchEngine
 import os
 import requests
 
@@ -6,10 +7,16 @@ load_dotenv('.env')
 
 WALKSCORE_KEY = os.environ.get('WALKSCORE_KEY')
 
+def get_zipcodes_by_city(city: str, state: str) -> list:
+    with SearchEngine() as search:
+        results = search.by_city_and_state(city='Louisville', state='Kentucky', returns=None)
+        return [int(result.zipcode) for result in results]
+
 def get_walkscore(lat: float, lon: float):
     response = requests.get(f'https://api.walkscore.com/score?format=json&lat={lat}&lon={lon}&transit=1&bike=1&wsapikey={WALKSCORE_KEY}')
     if response.ok:
         return response.json()
     return
 
+print(get_zipcodes_by_city('Louisville', 'Kentucky'))
 print(get_walkscore(38.328732, -85.764771))
