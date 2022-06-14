d1d9ffa2e2c7c59ea0fb24ac29310939dca4f095
Richard Oden <richard.thomas.oden@gmail.com>
2022-06-14 07:28:09 -0400

    Added some protection against invalid values in DesirabilityFactor and added score attribute

--- src/models/DesirabilityFactor.py
+++ src/models/DesirabilityFactor.py
@@ -1,4 +1,11 @@
 class DesirabilityFactor():
-    def __init__(self, value: int | float, importance: float):
+    def __init__(self, value: float, importance: float):
+        if value > 1 or value < 0:
+            raise ValueError(f'Value must be between 0 and 1. Current: {value}')
+
+        if importance > 1 or importance < 0:
+            raise ValueError(f'Importance must be between 0 and 1. Current: {importance}')
+
         self.value = value
         self.importance = importance
+        self.score = value * importance
