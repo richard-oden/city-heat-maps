ef209e1a108d35e243421cbcadb387ad63e68baf
Richard Oden <richard.thomas.oden@gmail.com>
2022-06-14 07:14:47 -0400

    Added ZipcodeDesirability.py in models module

---
+++ src/models/ZipcodeDesirability.py
@@ -0,0 +1,6 @@
+from models.DesirabilityProfile import DesirabilityProfile
+from uszipcode import ComprehensiveZipcode
+
+class ZipcodeDesirability():
+    def __init__(self, zipcode: ComprehensiveZipcode, desirability_profile: DesirabilityProfile):
+        pass
